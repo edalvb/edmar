2bef04f8ffd8c64c8756b68744568154fe696941
Edward Alexis <edalvb@gmail.com>
2021-11-08 23:16:32 -0500

    feature/develop: avance

---
+++ lib/core/model/alternativa.model.dart
@@ -0,0 +1,6 @@
+class Alternativa {
+  final int id;
+  final String name;
+
+  Alternativa({required this.id, required this.name});
+}
---
+++ lib/core/model/pregunta.model.dart
@@ -0,0 +1,8 @@
+import 'package:edmar/core/model/alternativa.model.dart';
+
+class Pregunta {
+  final int id;
+  final Alternativa alternativa;
+
+  Pregunta({required this.id, required this.alternativa});
+}
